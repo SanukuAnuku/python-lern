9f6a0d9956d48d13ff9c8a7b4fb75f1c6419bdb4
SanukuAnuku <sanukuanuku@gmail.com>
2026-09-07 09:31:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+"""Основной файл приложения Task Manager
+    version 0.0.1
+    -[] создать основной цикл
+"""
+is_start = True #flag
+
+while (is_start):
+    print('start')
+    is_starts = False
+    print('finish')
