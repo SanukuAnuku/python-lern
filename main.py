d884c51848ad92df16f0667e1aca5a509e31a514
SanukuAnuku <sanukuanuku@gmail.com>
2026-09-07 14:01:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 """Основной файл приложения Task Manager
-    version 0.0.1
-    -[] создать основной цикл
+    version 0.0.2
+    -[] реализовать место хранения задачь
+    -[] сделать функцию - показать заметки
+    -[] сделать функцию - добавить заметки
 """
+collection = ['task 1', 'task 2'] #Лист
 is_start = True #flag
 
 while (is_start):
-    print('start')
-    is_starts = False
-    print('finish')
+    print("1 - показать задачи | 2 - добавить заметку")
+    choice_user = input('ВВедите ваш выбор (1 или 2)')
+
+    match choice_user:
+        case '1':
+            print(collection)
+        case '2':
+            collection.append('task')
+            print(collection)
+        case _:
+            print('Такого пункста нет')
